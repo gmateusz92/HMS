ed1407dc4e263114775b206f45fd7c30a70e6dcd
mateusz <matek1600@o2.pl>
2022-10-19 12:26:12 +0200

    1

--- hotelbooking/urls.py
+++ hotelbooking/urls.py
@@ -6,7 +6,8 @@ app_name = 'hotelbooking'
 urlpatterns = [
     path('', views.home, name='home'),
     path('room_list/', RoomList.as_view(), name='RoomList'),
-    path('room_detail/<int:pk>', views.room_detail_view, name='room_detail.view'),
+    path('<int:room_pk>/', views.room_detail_view, name='room_detail_view'),
     path('booking_list/', BookingList.as_view(), name='BookingList'),
-    path('book/', BookingView.as_view(), name='booking_view')
+    path('book/', BookingView.as_view(), name='booking_view'),
+
 ]
