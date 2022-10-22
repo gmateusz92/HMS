1327a1a6162a7b0102eb0e839cf05d4f23e0e19a
Toshiba <gmateusz92@gmail.com>
2022-10-22 19:00:24 +0200

    1

---
+++ hotelbooking/booking_funkctions/get_room_cat_url_list.py
@@ -0,0 +1,17 @@
+from hotelbooking.models import Room
+from django.urls import reverse
+
+
+#funkcja zwracajaca room category i url liste
+
+def get_room_cat_url_list():
+    room = Room.objects.all()[0] #pobiera pierwszy losowy room object
+    room_categories = dict(room.ROOM_CATEGORIES)  # tworzy slownik z tuple
+    room_cat_url_list = [] # pusta lista category, URL
+
+
+    for category in room_categories:
+        room_category = room_categories.get(category) #room_category=value, category=key
+        room_url = reverse('hotelbooking:RoomDetailView', kwargs={'category': category})
+        room_cat_url_list.append((room_category, room_url))
+    return room_cat_url_list
